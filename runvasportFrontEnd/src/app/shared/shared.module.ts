3f02c844f054a1bb96b3fdb3f9f42fd5789f0d90
sergio <sergiomu.garcia@gmail.com>
2019-07-24 16:16:04 +0200

    implements templates

--- runvasportFrontEnd/src/app/shared/shared.module.ts
+++ runvasportFrontEnd/src/app/shared/shared.module.ts
@@ -3,18 +3,21 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
     declarations: [
         PagenotfoundComponent,
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        NavbarComponent
     ],
     exports: [
         PagenotfoundComponent,
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        NavbarComponent
     ]
 })
 export class SharedModule { }
