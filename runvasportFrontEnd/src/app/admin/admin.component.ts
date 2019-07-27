29831c5ef18755036d3bba34c555c1a595438711
sergio <sergiomu.garcia@gmail.com>
2019-07-27 20:22:30 +0200

    admin routes ok!

--- runvasportFrontEnd/src/app/admin/admin.component.ts
+++ runvasportFrontEnd/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateTablon() {
+    this.router.navigate(["admin/"]);
+  }
+
+  navigateEvents() {
+    this.router.navigate(["admin/eventos"]);
+  }
+
+  navigateCupons() {
+    this.router.navigate(["admin/cupones"]);
+  }
+
+  navigateUsers() {
+    this.router.navigate(["admin/usuarios"]);
   }
 
 }
