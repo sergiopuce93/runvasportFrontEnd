a106cd92af160345fc9a4a2dce075def83730cba
sergio <sergiomu.garcia@gmail.com>
2019-07-30 15:39:35 +0200

    Buscador tabla por cuqluier campo

--- runvasportFrontEnd/src/app/admin/eventos/eventos.component.ts
+++ runvasportFrontEnd/src/app/admin/eventos/eventos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventoService } from '../../services/evento.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-eventos',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
+  
+  // Variables
+  subscriptions: Subscription[] = [];
+  listEvents: any[] = [];
+  search: String;
 
-  constructor() { }
+  // Constructor
+  constructor(private eventoService: EventoService) { }
 
+  /**
+   * Init load
+   */
   ngOnInit() {
+    this.getEvents();
+  }
+
+  /**
+   * Destroy subscription
+   */
+  ngOnDestroy() {
+    this.subscriptions.forEach(element => {
+      element.unsubscribe();
+    })
+  }
+
+  /**
+   * Get all events
+   */
+  getEvents() {
+    this.listEvents = [];
+    this.subscriptions.push(
+      this.eventoService.getAllEvents().subscribe(response => {
+        this.listEvents = response;
+      })
+    );
   }
 
 }
