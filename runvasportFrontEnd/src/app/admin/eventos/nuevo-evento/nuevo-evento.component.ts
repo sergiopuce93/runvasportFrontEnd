a78d70dbec074d11cdc8b2eca44bff715287499f
sergio <sergiomu.garcia@gmail.com>
2019-09-27 12:49:30 +0200

    icon

--- runvasportFrontEnd/src/app/admin/eventos/nuevo-evento/nuevo-evento.component.ts
+++ runvasportFrontEnd/src/app/admin/eventos/nuevo-evento/nuevo-evento.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Evento } from '../../../interfaces/event.interface';
 
 @Component({
   selector: 'app-nuevo-evento',
@@ -6,8 +8,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevo-evento.component.css']
 })
 export class NuevoEventoComponent implements OnInit {
-
-  constructor() { }
+  formNewEvent: FormGroup;
+  newEvent: Evento = {
+    id: null,
+    name: null,
+    url: null,
+    imagen: null,
+    sport: null,
+    estimatedParticipants: null,
+    limitedParticipants: null,
+    dateIni: null,
+    dateEnd: null,
+    dateIniInscription: null,
+    dateEndInscription: null,
+    place: null,
+    address: null,
+    country: null,
+    provincia: null,
+    population: null,
+    postalCode: null,
+    email: null,
+    clasification: null,
+    limitedInscri: null
+  }
+  constructor() { 
+    this.formNewEvent = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      sport: new FormControl('', [
+        Validators.required
+      ]),
+      imagen: new FormControl('', [
+        Validators.required
+      ]),
+      estimatedParticipants: new FormControl('', [
+      ]),
+      limitedParticipants: new FormControl('', [
+      ]),
+      dateIni: new FormControl('', [
+      ]),
+      dateEnd: new FormControl('', [
+      ]),
+      dateIniInscription: new FormControl('', [
+      ]),
+      dateEndInscription: new FormControl('', [
+      ]),
+      place: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      address: new FormControl('', [
+      ]),
+      country: new FormControl('', [
+      ]),
+      provincia: new FormControl('', [
+      ]),
+      population: new FormControl('', [
+      ]),
+      postalCode: new FormControl('', [
+      ]),
+      limitedInscri: new FormControl('', [
+      ]),
+    });
+  }
 
   ngOnInit() {
 
