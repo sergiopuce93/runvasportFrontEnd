7740763ca6c4c91b8613563b13cee93c491d8301
sergio <sergiomu.garcia@gmail.com>
2020-03-21 13:06:49 +0100

    fixed event

--- runvasportFrontEnd/src/app/admin/eventos/nuevo-evento/nuevo-evento.component.ts
+++ runvasportFrontEnd/src/app/admin/eventos/nuevo-evento/nuevo-evento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ElementRef } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Evento } from '../../../interfaces/event.interface';
 import { EventoService } from '../../../services/evento.service';
@@ -10,53 +10,37 @@ import Swal from 'sweetalert2'
   styleUrls: ['./nuevo-evento.component.css']
 })
 export class NuevoEventoComponent implements OnInit {
+  labelImport: ElementRef;
+  fileToUpload: File = null;
   error: number;
   errorFormControl: String;
   formNewEvent: FormGroup;
   newEvent: Evento = {
     id: null,
-    nombre: null,
-    url: null,
+    name: null,
     imagen: null,
     sport: null,
-    estimatedParticipants: null,
-    limitedParticipants: null,
     dateIni: null,
     dateEnd: null,
-    dateIniInscription: null,
-    dateEndInscription: null,
     place: null,
     address: null,
     country: null,
     provincia: null,
-    population: null,
-    postalCode: null,
-    email: null,
-    clasification: null,
-    limitedInscri: null
   }
-  constructor(private eventService: EventoService) {
+  constructor(private eventService: EventoService, private cd: ChangeDetectorRef) {
     this.formNewEvent = new FormGroup({
-      nombre: new FormControl('', [
+      name: new FormControl('', [
         Validators.required,
         Validators.minLength(2),
       ]),
       sport: new FormControl('', [
         Validators.required
       ]),
-      imagen: new FormControl('', [
-      ]),
-      estimatedParticipants: new FormControl('', [
-      ]),
-      limitedParticipants: new FormControl('', [
+      image: new FormControl('', [
       ]),
       dateIni: new FormControl('', [
       ]),
       dateEnd: new FormControl('', [
-      ]),
-      dateIniInscription: new FormControl('', [
-      ]),
-      dateEndInscription: new FormControl('', [
       ]),
       place: new FormControl('', [
       ]),
@@ -65,12 +49,6 @@ export class NuevoEventoComponent implements OnInit {
       country: new FormControl('', [
       ]),
       provincia: new FormControl('', [
-      ]),
-      population: new FormControl('', [
-      ]),
-      postalCode: new FormControl('', [
-      ]),
-      limitedInscri: new FormControl('', [
       ]),
     });
   }
@@ -83,21 +61,15 @@ export class NuevoEventoComponent implements OnInit {
    * @param Evento
    */
   fillEvent(event: Evento) {
-    event.nombre = (this.formNewEvent.get('nombre').value).toUpperCase().trim();
+    event.name = (this.formNewEvent.get('name').value).toUpperCase().trim();
     event.sport = (this.formNewEvent.get('sport').value).toUpperCase().trim();
-    event.estimatedParticipants = this.formNewEvent.get('estimatedParticipants').value;
-    event.limitedParticipants = this.formNewEvent.get('limitedParticipants').value;
+    event.imagen = this.formNewEvent.get('image').value;
     event.dateIni = this.formNewEvent.get('dateIni').value;
     event.dateEnd = this.formNewEvent.get('dateEnd').value;
-    event.dateIniInscription = this.formNewEvent.get('dateIniInscription').value;
-    event.dateEndInscription = this.formNewEvent.get('dateEndInscription').value;
     event.place = (this.formNewEvent.get('place').value).toUpperCase().trim();
     event.address = (this.formNewEvent.get('address').value).toUpperCase().trim();
     event.country = (this.formNewEvent.get('country').value).toUpperCase().trim();
     event.provincia = (this.formNewEvent.get('provincia').value).toUpperCase().trim();
-    event.population = (this.formNewEvent.get('population').value).toUpperCase().trim();
-    event.postalCode = this.formNewEvent.get('postalCode').value;
-    event.limitedInscri = this.formNewEvent.get('limitedInscri').value
   }
 
   /**
@@ -149,52 +121,27 @@ export class NuevoEventoComponent implements OnInit {
       this.validateFormNewEvent();
     } else {
       this.fillEvent(this.newEvent);
-      this.eventService.newEvent(this.newEvent).subscribe(() => {
+      console.log(this.newEvent);
+      this.eventService.newEvent(this.newEvent).subscribe(data => {
+        Swal.fire({
+            type: 'success',
+            title: 'Evento creado'
+          })
       }, err => {
         if (err != null) {
           Swal.fire({
             type: 'error',
             title: 'Oops...',
-            text: 'Error al crear el evento',
+            text: err,
           })
         }
       });
     }
   }
-  // if (this.validateFormNewEvent() === true && this.errorForm === null) {
-  //   this.fillEvent(this.newEvent);
-  //   console.log(this.errorForm);
-  // this.eventService.newEvent(this.newEvent).subscribe(() => {
-  // }, err => {
-  //   if (err != null) {
-  //     Swal.fire({
-  //       type: 'error',
-  //       title: 'Oops...',
-  //       text: 'Error al crear el evento',
-  //     })
-  //   }
-  // });
-  // } else {
 
-  // }
-
-
-  /**
-  * FUNCION DESARROLLO
-  */
-  relleno() {
-    // this.formNewEvent.controls['nombre'].setValue("");
-    this.formNewEvent.controls['sport'].setValue("Running/Atletismo");
-    // this.formNewEvent.controls['estimatedParticipants'].setValue(1000);
-    // this.formNewEvent.controls['limitedParticipants'].setValue(1000);
-    // this.formNewEvent.controls['dateIni'].setValue("");
-    // this.formNewEvent.controls['dateEnd'].setValue("");
-    // this.formNewEvent.controls['dateIniInscription'].setValue("");
-    // this.formNewEvent.controls['dateEndInscription'].setValue("");
-    // this.formNewEvent.controls['place'].setValue("valladolid");
-    // this.formNewEvent.controls['country'].setValue("españa");
-    // this.formNewEvent.controls['postalCode'].setValue("47006");
-    // this.formNewEvent.controls['limitedInscri'].setValue(122);
+  handleFileInput(files: FileList) {
+    this.labelImport.nativeElement.innerText = files.item(0).name;
+    this.fileToUpload = files.item(0);
   }
 
 }
